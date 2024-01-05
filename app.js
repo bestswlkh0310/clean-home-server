1fed03a07157937d518c6187e4e4c16446f70265
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-05 11:47:50 +0900

    chore : more dummy

--- app.js
+++ app.js
@@ -15,12 +15,38 @@ app.use((req, res, next) => {
  * @createdAt: Date
  * @cost: Int
  */
-let items = [{
-  id: 1,
-  itemName: 'test1',
-  createdAt: '2024!',
-  cost: 100
-}];
+let items = [
+  {
+    id: 1,
+    itemName: 'test1',
+    createdAt: '2024!',
+    cost: 100
+  },
+  {
+    id: 2,
+    itemName: 'test2',
+    createdAt: '2024!',
+    cost: 200
+  },
+  {
+    id: 3,
+    itemName: 'test2',
+    createdAt: '2024!',
+    cost: 200
+  },
+  {
+    id: 4,
+    itemName: 'test2',
+    createdAt: '2024!',
+    cost: 200
+  },
+  {
+    id: 5,
+    itemName: 'test2',
+    createdAt: '2024!',
+    cost: 200
+  },
+];
 
 /**
  * - userModel
