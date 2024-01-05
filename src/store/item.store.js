bbfd6b46d1fda3123f84757fafe35ebe29c1e889
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-05 18:04:32 +0900

    delete : dummy

--- src/store/item.store.js
+++ src/store/item.store.js
@@ -5,35 +5,4 @@
  * @createdAt: Date
  * @cost: Int
  */
-export let items = [
-  {
-    id: 1,
-    itemName: 'test1',
-    createdAt: '2024!',
-    cost: 100
-  },
-  {
-    id: 2,
-    itemName: 'test2',
-    createdAt: '2024!',
-    cost: 200
-  },
-  {
-    id: 3,
-    itemName: 'test2',
-    createdAt: '2024!',
-    cost: 200
-  },
-  {
-    id: 4,
-    itemName: 'test2',
-    createdAt: '2024!',
-    cost: 200
-  },
-  {
-    id: 5,
-    itemName: 'test2',
-    createdAt: '2024!',
-    cost: 200
-  },
-];
+export let items = [];
